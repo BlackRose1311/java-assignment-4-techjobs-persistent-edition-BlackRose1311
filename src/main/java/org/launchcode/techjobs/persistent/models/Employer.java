f1e0c7853dc30efb39e4cbfb56277164d36ffcd8
BlackRose1311 <mr.powers1311@gmail.com>
2023-06-11 15:07:11 -0500

    finally finished part 3 - failing sql test - need to add code for it

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,10 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
     @NotBlank(message = "Location is required.")
     @Size(min = 3, max = 100, message = "Location must be between 3 and 100 characters.")
     private String location;
